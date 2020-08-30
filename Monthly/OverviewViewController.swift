2ca022fde7a138ae8f34c58c944bf2a30f61d0ee
Kobe McKee <kobe.mckee@gmail.com>
2020-08-30 13:16:19 -0600

    Set up views with corresponding files

--- Monthly/OverviewViewController.swift
+++ Monthly/OverviewViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
-class OverviewViewController: UIViewController {
-
+class OverviewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var displayView: UIView!
+    @IBOutlet weak var paymentTableView: UITableView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    // MARK: Payment TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
+
