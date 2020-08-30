c6723ad5525a8ce7449271ea1687358cfe04bf00
Kobe McKee <kobe.mckee@gmail.com>
2020-08-30 12:43:05 -0600

    Initial Commit

---
+++ Monthly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Monthly
+//
+//  Created by Kobe McKee on 8/30/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
