565ea2cfecb087feb396d750dda446973654a9b9
Kobe McKee <kobe.mckee@gmail.com>
2020-09-23 11:30:11 -0600

    Set up repeat options table view

--- Monthly/RepeatViewController.swift
+++ Monthly/RepeatViewController.swift
@@ -8,15 +8,59 @@
 
 import UIKit
 
-class RepeatViewController: UIViewController {
+class RepeatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var repeatOptionTableView: UITableView!
+    
+    let repeatOptions = ["Never",
+                         "Daily",
+                         "Weekly",
+                         "Biweekly",
+                         "Monthly",
+                         "Every 3 Months",
+                         "Every 6 Months",
+                         "Yearly"]
+    
+    var selectedOption: String? = "Never"
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        repeatOptionTableView.dataSource = self
+        repeatOptionTableView.delegate = self
+        repeatOptionTableView.layer.cornerRadius = 5
     }
     
 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repeatOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repeatOptionCell", for: indexPath)
+        
+        let option = repeatOptions[indexPath.row]
+        cell.textLabel?.text = option
+        if option == selectedOption {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedCell = repeatOptions[indexPath.row]
+        selectedOption = selectedCell
+        repeatOptionTableView.reloadData()
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
