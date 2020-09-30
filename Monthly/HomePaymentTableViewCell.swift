7fc7d0c1c59d8916040c81eba1b0e5a047262bf2
Kobe McKee <kobe.mckee@gmail.com>
2020-09-30 11:14:32 -0600

    Added app icon v1

--- Monthly/HomePaymentTableViewCell.swift
+++ Monthly/HomePaymentTableViewCell.swift
@@ -14,6 +14,8 @@ class HomePaymentTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
