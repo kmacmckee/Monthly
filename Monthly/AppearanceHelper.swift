067a6a4446d0f0c8476f9af64eff7d301819a973
Kobe McKee <kobe.mckee@gmail.com>
2020-09-30 11:48:00 -0600

    Added category icons. Created Appearance Helper

---
+++ Monthly/AppearanceHelper.swift
@@ -0,0 +1,19 @@
+//
+//  AppearanceHelper.swift
+//  Monthly
+//
+//  Created by Kobe McKee on 9/30/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum AppearanceHelper {
+    
+    static let billsIcon = UIImage(named: "BillsIcon")
+    static let entertainmentIcon = UIImage(named: "EntertainmentIcon.2")
+    static let foodIcon = UIImage(named: "FoodIcon")
+    static let incomeIcon = UIImage(named: "IncomeIcon")
+       
+}
